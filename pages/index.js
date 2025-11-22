54a46867a1053cc4a1144f562be995595a299e41
Wilmer Polanco <120139551+wilmerp13@users.noreply.github.com>
2025-11-22 14:28:40 -0500

    Refactor import statements in index.js

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,7 @@ import SkillRadar from '../components/SkillRadar'
 import Link from 'next/link'
 import Skills from "../components/Skills" // <-- Import your Skills component
 import CaseStudies from "../components/CaseStudies" // optional, if you want case studies
-import Experience from "../components/Experience"
-
-
-
+import Experience from "../components/Experience" 
 
 const skillsData = [
   { subject: 'Networking', A: 85 },
